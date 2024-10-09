0809cdda2ccaf5c416696ee37b41eec2ee242479
dev-rfgul <rfgul587@gmail.com>
2024-10-09 12:56:40 +0500

    completed part 4 of the videos, but the postman is not working till here

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,8 +1,12 @@
 const mongoose=require('mongoose');
+const config=require('config');
 
-mongoose.connect('mongodb://127.0.0.1/27017/scatch')
+const dbgr= require('debug')("development:mongoose")
+
+mongoose.connect(`${config.get("MONGODB_URI")}/scatch`)
 .then(()=>{
-    console.log("Connected to database");
+    // console.log("Connected to database");
+    dbgr("Connected to database");
 })
 .catch((err)=>{
     console.log("error ::" + err); 
--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -1,8 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const ownerModel = require('../models/owner-model')
 
-router.get('/',(req,res)=>{
+router.get('/', (req, res) => {
     res.send("hello world");
 })
 
-module.exports=router;
+console.log(process.env.NODE_ENV);
+// i have initialized the environments variables of node to development and it will check the route if its development then the create route will not be pushed
+
+if (process.env.NODE_ENV === 'development') {
+
+    router.post('/create', async (req, res) => {
+        let owner = await ownerModel.find();
+        if (owner.length > 0) return res.status(503).send("You dont have the rights to create a new Owner")
+        res.send("we can create owner") 
+        let { fullName, email, password } = req.body;
+        let createdOwner = await ownerModel.create({
+            fullName,
+            email,
+            password,
+        })
+        res.status(201).send(createdOwner);
+    })
+}
+
+module.exports = router;
