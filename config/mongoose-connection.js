acbf4308b52028a85fad82592243b5bf019f7832
dev-rfgul <rfgul587@gmail.com>
2024-10-15 22:54:01 +0500

    tried to continue the project but teh errors are still the same

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,15 +1,39 @@
-const mongoose=require('mongoose');
-const config=require('config');
+// const mongoose=require('mongoose');
+// const config=require('config');
 
-const dbgr= require('debug')("development:mongoose")
+// const dbgr= require('debug')("development:mongoose")
 
-mongoose.connect(`${config.get("MONGODB_URI")}/scatch`)
-.then(()=>{
-    // console.log("Connected to database");
+// mongoose.connect(`${config.get("MONGODB_URI")}/scatch`, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+// }).then(()=>{
+//     // console.log("Connected to database");
+//     dbgr("Connected to database");
+// })
+// .catch((err)=>{
+//     console.log("error ::" + err); 
+// })
+
+// module.exports=mongoose.connection;
+
+
+const mongoose = require('mongoose');
+const config = require('config');
+const dbgr = require('debug')("development:mongoose");
+
+// Ensure the URI is correct and doesn't have unwanted characters
+const uri = `${config.get("MONGODB_URI")}/scatch`;
+
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => {
     dbgr("Connected to database");
 })
-.catch((err)=>{
-    console.log("error ::" + err); 
-})
+.catch((err) => {
+    console.error("Connection error: ", err); 
+});
 
-module.exports=mongoose.connection;
+// Export the connection
+module.exports = mongoose.connection;
